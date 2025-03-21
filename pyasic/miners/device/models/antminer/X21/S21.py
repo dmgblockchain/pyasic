2538f62c344d76043a2d6ea363a4262ca6acf666
Adrian Glover <adrian.glover@gmail.com>
2025-03-20 18:02:27 -0700

    Corrected model string

--- pyasic/miners/device/models/antminer/X21/S21.py
+++ pyasic/miners/device/models/antminer/X21/S21.py
@@ -55,7 +55,7 @@ class S21Hydro(AntMinerMake):
 
 
 class S21PlusHydro(AntMinerMake):
-    raw_model = MinerModel.ANTMINER.S21Hydro
+    raw_model = MinerModel.ANTMINER.S21PlusHydro
 
     expected_chips = 95
     expected_hashboards = 3
